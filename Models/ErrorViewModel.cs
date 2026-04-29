8c9426d6e731a858ae21bebbe548958dce6d8448
Muhammad Ikram <muhammadikram@maxsys.com.my>
2026-04-29 11:36:20 +0800

    init net8 base framework

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tms_template_net8.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
